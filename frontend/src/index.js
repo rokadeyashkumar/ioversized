8e2269cf0fa4c02364c083eb4942f7d3148ab960
yashTMG <yashkumar.rokade@themaryadagroup.com>
2024-06-21 01:50:08 +0530

    add basic folder and setup

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+// frontend/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Optional: Import your CSS/SASS here
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
